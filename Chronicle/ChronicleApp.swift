dd888b07b206e21b92b4f74de899b76a55f6444b
Chris Morrell <inxilpro@users.noreply.github.com>
2026-09-01 09:55:04 -0400

    Initial Commit

---
+++ Chronicle/ChronicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronicleApp.swift
+//  Chronicle
+//
+//  Created by Chris Morrell on 9/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ChronicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChronicleTests/ChronicleTests.swift
@@ -0,0 +1,19 @@
+//
+//  ChronicleTests.swift
+//  ChronicleTests
+//
+//  Created by Chris Morrell on 9/1/26.
+//
+
+import Testing
+@testable import Chronicle
+
+struct ChronicleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
